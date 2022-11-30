94a9d85f45d423cf39ea4a4f417e4826aa6ecf90
EPICGAMING191 <112524668+EPICGAMING191@users.noreply.github.com>
2022-11-29 20:29:02 -0500

    Add files via upload

---
+++ PlayerController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class PlayerController : MonoBehaviour
+{
+private float speed = 20.0f;
+private float turnSpeed = 45.0f;
+private float horizontalInput;
+private float forwardInput;
+void Start{}
+{
+//Run when start	
+}
+void Update()
+{
+//Run repeatedly
+horizontalInput = Input.GetAxis("Horizontal");
+forwardInput = Input.GetAxis("Vertical")
+//Move the vehicle forward
+transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+
+}
+
+}
