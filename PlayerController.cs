2ee9fd841b025fb3b7710103f535c8f347c00cfe
EPICGAMING191 <112524668+EPICGAMING191@users.noreply.github.com>
2022-11-30 08:42:54 -0500

    Add files via upload

--- PlayerController.cs
+++ PlayerController.cs
@@ -1,8 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-public class PlayerController : MonoBehaviour
-{
+public class PlayerController : MonoBehaviour {
 private float speed = 20.0f;
 private float turnSpeed = 45.0f;
 private float horizontalInput;
